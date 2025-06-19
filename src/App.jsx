7f2489d322ee9c477ce261b18212eaad54f508b3
ryantsany <ryantsany2003@gmail.com>
2025-06-19 18:47:19 +0700

    template

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+function App() {
+  return (
+    <div className="min-h-svh flex-col items-center justify-center">
+      <DropdownMenu>
+        <DropdownMenuTrigger className="bg-primary text-primary-foreground px-5 py-5 rounded-2xl cursor-pointer">Open</DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>My Account</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem>Billing</DropdownMenuItem>
+          <DropdownMenuItem>Team</DropdownMenuItem>
+          <DropdownMenuItem>Subscription</DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </div>
+  )
+}
+
+export default App
